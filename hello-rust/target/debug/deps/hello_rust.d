a2f3d79ba99555581e64f799f90749c20fe0e468
flatfisher <shimano.entou@gmail.com>
2021-07-15 01:04:55 +0900

    hello world

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+/Users/kyohei.tsuda/Documents/git/rust/hello-rust/target/debug/deps/hello_rust: src/main.rs
+
+/Users/kyohei.tsuda/Documents/git/rust/hello-rust/target/debug/deps/hello_rust.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/kyohei.tsuda/Documents/git/rust/hello-rust/target/debug/hello-rust: /Users/kyohei.tsuda/Documents/git/rust/hello-rust/src/main.rs
